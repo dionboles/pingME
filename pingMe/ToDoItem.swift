84735206131aa61ba0ef0ffa7b4a309d491aa8a9
Dion Boles <dboles@aamiras-iMac-2.home>
2017-12-27 17:14:36 -0500

    work on it later

---
+++ pingMe/ToDoItem.swift
@@ -0,0 +1,13 @@
+//
+//  ToDoItem.swift
+//  pingMe
+//
+//  Created by Dion Boles on 12/27/17.
+//  Copyright © 2017 Dion Boles. All rights reserved.
+//
+
+import UIKit
+
+class ToDoItem: NSObject {
+
+}
